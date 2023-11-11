ddd28a3ed240a4ef21a469376577e134b42bb4d8
minamagdi <mina.m.halim642@gmail.com>
2023-11-11 11:56:05 +0200

    add driver factory

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,20 @@
 package base;
 
+import driverFactory.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
 public class BaseTest {
+	protected WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+		driver = DriverFactory.driverInit();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+		driver.get("https://qacart-todo.herokuapp.com/");
+	}
 }
