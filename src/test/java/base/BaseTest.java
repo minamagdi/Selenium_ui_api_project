69f5ffe3a60bb1b4efae76e8853efbee4ac8e5b1
minamagdi <mina.m.halim642@gmail.com>
2023-11-11 13:02:16 +0200

    add login tests and property files

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,12 +3,13 @@ package base;
 import driverFactory.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
+import pages.LoginPage;
 
 import java.time.Duration;
 
 public class BaseTest {
 	protected WebDriver driver;
-
+    protected LoginPage loginPage;
 	@BeforeMethod
 	public void setUp() {
 		driver = DriverFactory.driverInit();
@@ -16,5 +17,6 @@ public class BaseTest {
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
 		driver.get("https://qacart-todo.herokuapp.com/");
+		loginPage = new LoginPage(driver);
 	}
 }
