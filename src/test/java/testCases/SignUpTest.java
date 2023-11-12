fffc3727f4f07a7db2001a2008576f88ef27e3d8
minamagdi <mina.m.halim642@gmail.com>
2023-11-12 13:29:22 +0200

    add new assertion to signup test

--- src/test/java/testCases/SignUpTest.java
+++ src/test/java/testCases/SignUpTest.java
@@ -1,6 +1,7 @@
 package testCases;
 
 import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignUpTest extends BaseTest {
@@ -8,6 +9,8 @@ public class SignUpTest extends BaseTest {
 	@Test
 	public void signUpWithValidData() {
 		homePage
-				.signUp("adhm","mohamed","adhmed@test.com","Adham123456");
+				.signUp("adhm","mohamed","adhmed2@test.com","Adham123456");
+
+		Assert.assertEquals(todoPage.getWelcomeMessage("adhm"),"GOOD MORNING ADHM");
 	}
 }
